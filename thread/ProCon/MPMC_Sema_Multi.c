e5e0e45aad8f16e5db2e1e4e8ca22d8520e7fe7c
lin3ux <yyn_linlin@foxmail.com>
2022-05-08 22:01:30 +0800

    生产者消费者的信号量实现

---
+++ thread/ProCon/MPMC_Sema_Multi.c
@@ -0,0 +1,97 @@
+/*
+使用信号量完成生产者消费者问题
+多生产者多消费者
+总资源数为大于1：会出现多个线程同时访问共享资源的混乱情况，需要加互斥锁
+    - 多个生产者同时生产
+    - 多个消费者同时消费
+    - 生产者和消费者同时运行
+省略检查返回值
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+struct Node {
+    int number;
+    struct Node *next;
+};
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+struct Node *head = NULL;
+sem_t psem;//生产者信号量
+sem_t csem;//消费者信号量
+
+
+void *producer(void *arg);//生产者函数
+void *consumer(void *arg);//消费者函数
+
+int main(int argc, char **argv)
+{
+    pthread_t ptid[5];//生产者线程
+    pthread_t ctid[5];//消费者线程
+
+    sem_init(&psem, 0, 5);//初始化生产者信号量，生产者线程拥有1个信号灯
+    sem_init(&csem, 0, 0);//初始化消费者信号量，消费者线程拥有0个信号灯
+    //如此这般的意义是使生产者线程先运行
+
+    for(int i = 0; i < 5; i++){//初始化线程
+        pthread_create(&ptid[i], NULL, producer, NULL);
+        pthread_create(&ctid[i], NULL, consumer, NULL);
+    }
+    
+    sem_destroy(&psem);//反初始化信号量
+    sem_destroy(&csem);
+    pthread_mutex_destroy(&mutex);
+    
+    for(int i = 0; i < 5; i++){//回收线程
+        pthread_join(ptid[i], NULL);
+        pthread_join(ctid[i], NULL);
+    }
+
+    return 0;
+}
+
+void *producer(void *arg)
+{
+    while(1){
+        sem_wait(&psem);//生产者拿一个信号灯
+        pthread_mutex_lock(&mutex);
+        //上面两句顺序不能颠倒，要不然会产生死锁
+
+        struct Node* pnew = (struct Node*)malloc(sizeof(struct Node));//创建一个新节点
+        pnew -> number = rand() % 100;//初始化
+        pnew -> next = head;//链接节点
+        head = pnew;//头结点前移
+        printf("Producer: number = %d, tid = %ld\n", pnew -> number, pthread_self());
+        pthread_mutex_unlock(&mutex);
+
+        sem_post(&csem);//通知消费者消费
+        sleep(rand() % 3);
+    }
+
+    return NULL;
+}
+
+void *consumer(void *arg)
+{
+    while(1){
+        sem_wait(&csem);
+        pthread_mutex_lock(&mutex);
+        //以上两句顺序不可颠倒
+
+        struct Node *pnode = head;//生产者先运行确保此处节点挪动逻辑不会出错
+        printf("Consumer: number = %d, tid = %ld\n", pnode -> number, pthread_self());
+        head = pnode -> next;
+        free(pnode);
+        pthread_mutex_unlock(&mutex);
+
+        sem_post(&psem);//通知生产者生产
+
+        sleep(rand() % 3);
+    }
+
+    return NULL;
+}
